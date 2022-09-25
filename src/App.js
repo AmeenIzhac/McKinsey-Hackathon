e3d5194e889118bc2fa4ec23faf7cc11a2d16c8f
ameen <ameen.izhac@gmail.com>
2022-09-25 04:49:12 +0100

    have this

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LandingPage';
 import Campaigns from './components/Campaigns';
-<<<<<<< HEAD
-import "bootstrap/dist/css/bootstrap.min.css"
-=======
->>>>>>> 4045f9ba21d36181bf5c4ba07b3ecddb2561fb0c
+// import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
 function App() {
