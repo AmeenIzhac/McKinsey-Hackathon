a611fa210daad034f1ecea927ff02b181ceb9cc4
ameen <ameen.izhac@gmail.com>
2022-09-25 08:46:41 +0100

    dw

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LandingPage';
 import Campaigns from './components/Campaigns';
+import VirtualReality from './components/VirtualReality';
 // import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
       <Route path="/" exact element={<LoginForm />} />
       <Route path="/campaigns" exact element={<Campaigns/>} />
+      <Route path="/virtualReality" exact element={<VirtualReality/>} />
       </Routes>
     </Router>
     </>
--- src/components/campaignWidget.js
+++ src/components/campaignWidget.js
@@ -1,14 +1,17 @@
 import './CampaignWidget.css'
+import {Link} from 'react-router-dom'
 function CampaignWidget({campaignImg, progress, campaignText}) {
     return (
         <>
-        <div className="campaignWidgetBorder">
-            <div className='campaignWidgetImageBorder'>
-            <img className="campaignWidgetImage" src={campaignImg} alt="dw"></img>
+        <Link to={'/virtualReality'}>
+            <div href="/virtualReality" className="campaignWidgetBorder">
+                <div className='campaignWidgetImageBorder'>
+                <img className="campaignWidgetImage" src={campaignImg} alt="dw"></img>
+                </div>
+                <div className="progressBar"><div className="progress" style={{width: progress}}></div></div>
+                <p className="campaignWidgetText">{campaignText}</p>
             </div>
-            <div className="progressBar"><div className="progress" style={{width: progress}}></div></div>
-            <p className="campaignWidgetText">{campaignText}</p>
-        </div>
+        </Link>
         </>
     )
 }
